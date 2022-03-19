067de81bf74438c2a739d9dccaafab9d3a298e54
zuugnn <zuugnn@gmail.com>
2022-03-19 14:42:37 +0900

    m

--- aptCate.js
+++ aptCate.js
@@ -1,28 +1,14 @@
-$(document).ready(function () {
-    $("#liarea").on("click", "li a", function () {
-        var area = $(this).text();
-        $("#room").prev().removeClass("disabled");
-        $("#liarea")
-            .prev()
-            .html(area + ' <span class="caret"></span>');
+var states = ["Kansas", "Oklahoma", "Texas"];
 
-        // call ajax here using area var as post data
-
-        // Demo data
-        var ajaxData = ["room 1", "room 2", "room 3"];
-        var i, len;
-        for (i = 0, len = ajaxData.length; i < len; i++) {
-            $("#room").append('<li role="presentation"><a role="menuitem" tabindex="-1" href="#">' + ajaxData[i] + "</a></li>");
-        }
-    });
-
-    $("#room").on("click", "li a", function () {
-        var room = $(this).text();
-        $("#room")
-            .prev()
-            .html(room + ' <span class="caret"></span>');
-
-        // any ajax call here
-        alert(room + " selected");
-    });
+$(".dropdown-menu > li > a").click(function () {
+    yourFunction(this.text);
 });
+
+function yourFunction(val) {
+    if (val === "Action") {
+        $("#states").html("");
+        for (var i in states) {
+            $("#states").append("<li><a href='#'>" + states[i] + "</a></li>");
+        }
+    }
+}
